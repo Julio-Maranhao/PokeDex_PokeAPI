e03daaf1d20e612372b3af612418fc7fd7fb80e5
Julio Maranhao <julio.maranhao@outlook.com>
2023-04-03 09:20:17 -0300

    commit pokemon card

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
 const pokemonList = document.getElementById('pokemonList');
 const loadMoreButton = document.getElementById('loadMoreButton');
+const pokemonCard = document.getElementById('pokemon-card');
+const backgroundMask = document.getElementById('background-mask');
+const returnButton = document.getElementById('return-button');
+const likeButton = document.getElementById('like-button')
+
+
+// add lista de likes
+let likePokemons = [];
 // limite dos pokemon que vão aparecer na busca
 const maxSearch = 151;
 // dados para ajustar a busca
@@ -27,18 +35,62 @@ function getPokemonColor (pokemon) {
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.color.name)
     .then((colorName) => {
-        li_element.classList.add(colorName)
+        li_element.classList.add(colorName);
+        if (likePokemons.includes(pokemon.name)) {
+            let li_number = li_element.getElementsByClassName('number')[0];
+            li_number.classList.add('liked-li');
+            li_number.style.opacity = "0.7";
+            
+        }
     })
 }
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = [])=> {
-        console.log(pokemons)
         const newli = pokemons.map(convertPokemonToLi).join('')
         pokemonList.innerHTML += newli
         pokemons.map(getPokemonColor)
     })
 }
+
+function addPokemonLikes() {
+    const li_elements = document.getElementsByClassName('pokemon')
+    for (pokemon of li_elements) {
+        let pokemon_name = pokemon.getElementsByClassName('name')[0].innerHTML
+        let pokemon_number = pokemon.getElementsByClassName('number')[0]
+        if (likePokemons.includes(pokemon_name)) {
+            pokemon_number.classList.add('liked-li');
+            pokemon_number.style.opacity = "0.7";
+        }
+    }
+}
+
+function removePokemonLikes() {
+    const li_elements = document.getElementsByClassName('pokemon')
+    for (pokemon of li_elements) {
+        let pokemon_name = pokemon.getElementsByClassName('name')[0].innerHTML
+        let pokemon_number = pokemon.getElementsByClassName('number')[0]
+        if (!likePokemons.includes(pokemon_name)) {
+            pokemon_number.classList.remove('liked-li');
+            pokemon_number.style.opacity = "0.3";
+        }
+    }
+}
+
+// Shows a card with the pokemon status
+pokemonList.addEventListener('DOMSubtreeModified', () => {
+    pokemonList.querySelectorAll('.pokemon').forEach((li) => {
+        li.addEventListener('click', () => {
+            let pokemon_name = li.querySelector('.name').innerHTML;
+            backgroundMask.classList.add('show-card');
+            // Inserir O elemento Loading
+            // INSERIR A FUNÇÃO SHOW  POKEMON CARD
+            pokemonCard.classList.add('show-card');
+
+            console.log(pokemon_name);
+        })
+    })
+})
 
 loadPokemonItens(offset, limit)
 
@@ -53,4 +105,30 @@ loadMoreButton.addEventListener('click', () => {
     } else {
         loadPokemonItens(offset, limit)
     }
+})
+
+//pokemonCard.addEventListener('click', ()=> console.log('clicou dentro'))
+backgroundMask.addEventListener('click', ()=> {
+    pokemonCard.classList.remove('show-card');
+    backgroundMask.classList.remove('show-card');
+})
+
+returnButton.addEventListener('click', () => {
+    pokemonCard.classList.remove('show-card');
+    backgroundMask.classList.remove('show-card');
+})
+
+likeButton.addEventListener('click', () => {
+    let cardName = pokemonCard.getElementsByClassName('card-name')[0].innerHTML
+    if (!likePokemons.includes(cardName)) {
+        likePokemons.push(cardName);
+        addPokemonLikes();
+        console.log(likePokemons)
+        likeButton.getElementsByClassName('like-icon')[0].classList.add('liked');
+    } else {
+        likePokemons.splice(likePokemons.indexOf(cardName), 1);
+        removePokemonLikes();
+        likeButton.getElementsByClassName('like-icon')[0].classList.remove('liked');
+    }
+    
 })
---
+++ assets/js/pokemon_card.js
@@ -0,0 +1,59 @@
+/*
+
+
+--->>> EVOLUTION CHAIN
+
+- ANTERIOR:
+    first pokemon of chain is not the pokemon we are looking for
+    search every chain in evolves_to for the pokemon we are wanting
+
+- CHAIN - evolves_to if not vazio - > evolves_to if not vazio - > evolves-to if not vazio
+
+- NUMBER - evolve_to species if id <= 151
+
+- TYPE - evolution_details trigger level-up, trade, use-item
+
+- MULTI horizontal:
+    evolves_to has more than one chain.
+
+
+
+- TEST CASES:
+19 raticate
+25 pikachu
+27 sandsrew
+35 clefary
+37 vulpix
+39 jigglypuff
+41 zubat
+43 oddish
+50 digglet
+52 meowth
+56 mankey
+58 arcanine
+60 poliwag
+74 geodude
+77 ponyta
+79 slowpoke
+81 mangemite
+88 grimer
+95 onix
+100 voltorb
+102 Exeggcute
+104 cubone
+106 hitmonlee
+107 hitmonchan
+108 lickitung
+109 koffing
+111 rhyhorn
+113 chansey
+114 tangela
+122 mr mime
+123 scyther
+124 jynx
+125 electabuzz
+126 magmar
+133 eeve
+137 porygon
+143 snorlax
+*/
